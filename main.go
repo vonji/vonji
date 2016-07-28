6c5975346329439337865e294ccfc2f29835f9f2
mantal <mantal0@outlook.fr>
2016-07-28 12:18:34 +0200

    Ensure that the database contain at least one user

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/rs/cors"
 
 	"github.com/vonji/vonji-api/models"
+	"github.com/vonji/vonji-api/services"
 )
 
 //TODO dependecy injection?
@@ -25,7 +26,6 @@ func main() {
 	db.LogMode(true)
 
 	defer db.Close()
-	db.AutoMigrate(&models.Tag{}, &models.User{}, &models.Request{}, &models.Response{}, &models.Comment{})
 
 	if err != nil {
 		panic(err.Error())
@@ -45,6 +45,21 @@ func main() {
 	api.InitContext(&app, db)
 	routes.RegisterRoutes(r)
 
+	initDB(db)
+
 	//TODO use something like Alice to chain middlewares
 	http.ListenAndServe(":1618", handlers.LoggingHandler(os.Stdout, c.Handler(r)))
+}
+
+func initDB(db *gorm.DB) {
+	db.AutoMigrate(&models.Tag{}, &models.User{}, &models.Request{}, &models.Response{}, &models.Comment{})
+	if len(services.User.GetAll()) == 0 {
+		services.User.Create(&models.User{
+			Email: "admin@vonji.fr",
+			Password: "admin",
+			FirstName: "Admin",
+			LastName: "Admin",
+			Description: "THE ALMGIGHTY ONE",
+		})
+	}
 }
