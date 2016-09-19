a45a09fbfd898fff3a252c56ee4567f04df96a78
mantal <mantal0@outlook.fr>
2016-09-19 12:02:51 +0200

    VON-31 VON-33 Add duration, frequenty and period to requests

--- models/request.go
+++ models/request.go
@@ -9,5 +9,10 @@ type Request struct {
 	Responses []Response
 	Views     uint
 	Tags      []Tag `gorm:"many2many:request_tags;"`
-	Status     string
+	Status    string
+	Duration  uint
+	Frequency uint
+	FrequencyUnit string
+	PeriodStart  string
+	PeriodEnd    string
 }
