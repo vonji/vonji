e3a0e1e88f82f72bcc3167018642f38570e2cb18
Loup Peluso <loup.peluso@gmail.com>
2016-11-19 12:10:49 +0100

    Setup bdd

--- src/main.js
+++ src/main.js
@@ -3,8 +3,27 @@
  */
 
 import express from 'express';
+import knexFactory from 'knex';
+import bookshelfFactory from 'bookshelf';
 
 const app = express();
+
+const knex = knexFactory({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'vonji-api',
+        password: 'vonji-api',
+        database: 'vonji-api',
+        charset: 'utf8'
+    }
+});
+
+const bookshelf = bookshelfFactory(knex);
+
+const User = bookshelf.Model.extend({
+    tableName: 'users',
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
