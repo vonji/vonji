3ab4619d2f3d1ff36a72fe4fc56b57a368178506
Loup Peluso <loup.peluso@gmail.com>
2016-11-19 11:18:00 +0100

    Setup Hello world!

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)(); /**
+                                     * Created by loup on 19/11/16.
+                                     */
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
