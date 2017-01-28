a847f2a7a779f98993fa1f42e365103dbbd92472
Loup Peluso <loup.peluso@gmail.com>
2017-01-28 10:57:02 +0100

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const babel = require('gulp-babel');
 gulp.task('default', () => {
 	return gulp.src('src/main.js')
 		.pipe(babel({
-			presets: ['es2015']
+			presets: ['es2015', 'stage-2'],
+      plugins: ['transform-runtime'], 
 		}))
 		.pipe(gulp.dest('dist'));
 });
